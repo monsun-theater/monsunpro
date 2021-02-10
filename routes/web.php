2f0fa6068d1d6f8b0b0cbb554977d6591a440fcb
jkaliski <jjkaliski@gmail.com>
2021-02-10 14:28:39 +0100

    Mobile search added, Search results template added

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::statamic('example', 'example-view', [
-//    'title' => 'Example'
-// ]);
+Route::statamic('/search', 'search', [
+    'title' => 'Suchergebnisse'
+ ]);
 
 Route::get('/!/DynamicToken/refresh', 'DynamicToken@getRefresh');
 Route::statamic('/sitemap.xml', 'sitemap/sitemap', ['layout' => null, 'content_type' => 'application/xml']); 
