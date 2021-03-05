582daa24ba3f1ff03314665e4a1dc47f118caae7
jkaliski <jjkaliski@gmail.com>
2021-03-05 13:28:03 +0100

    translation strings added

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Passwort zurückgesetzt!',
+    'sent' => 'Link zum Zurücksetzen Ihres Passworts zugesendet.',
+    'throttled' => 'Bitte warten und später noch Mal versuchen.',
+    'token' => 'Passwort-Reset-Token ist ungültig.',
+    'user' => "Benutzer mit dieser E-Mail-Adresse nicht gefunden.",
+
+];
---
+++ resources/lang/de/strings.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    // Pagination
+    'of'                                => 'von',
+    'previous'                          => 'vorherige',
+    'next'                              => 'nächste',
+
+    // Contact form
+    'form_honeypot'                     => 'Fax',
+    'form_mail_body_owner'              => 'Ihre Kontaktformular wurde uns gesendet.',
+    'form_mail_body_sender'             => 'Vielen Dank für Ihre Nachricht. Wir werden Sie so schnell wie moeglich kontaktieren.',
+    'form_mail_closing'                 => 'Mit freundlichen Grüßen',
+    'form_mail_from'                    => config('app.name'),
+    'form_mail_greeting'                => '',
+    'form_mail_logo'                    => 'https://unavatar.now.sh/jjkaliski@gmail.com',
+    'form_mail_submitted'               => 'Formular gesendet',
+    'form_mail_title'                   => 'Kontaktformular',
+    'form_mail_url'                     => config('app.url'),
+    'form_send'                         => 'Schicken',
+    'form_success'                      => 'Vielen Dank!',
+    
+    // Search
+    'search'                            => 'Suche',
+    'search_no_results'                 => 'Keine Suchergebnisse gefunden.',
+    'search_results_for'                => 'Suchergebnisse für',
+    
+    // Other
+    'set_dark_mode'                     => 'Dark-Modus aktivieren',
+    'disable_dark_mode'                 => 'Dark-Modus deaktivieren',
+];
