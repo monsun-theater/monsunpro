6b44c3d44ad80b96402dcced740deb662e9c7fe5
tom300z <tom@hoerberg.de>
2025-08-22 17:53:12 +0200

    Skip gh actions on bot commit

--- config/statamic/git.php
+++ config/statamic/git.php
@@ -115,7 +115,7 @@ return [
 
     'commands' => [
         'git add {{ paths }}',
-        'git -c "user.name={{ name }}" -c "user.email={{ email }}" commit -m "{{ message }} [BOT]"',
+        'git -c "user.name={{ name }}" -c "user.email={{ email }}" commit -m "{{ message }} [BOT] [skip ci]"',
     ],
 
     /*
