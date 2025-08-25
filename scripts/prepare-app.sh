34ebb1833d3604a1e2b9271abaf04c49d40bbab0
tom300z <tom@hoerberg.de>
2025-08-25 11:29:56 +0200

    Hardcode docroot using sed

--- scripts/prepare-app.sh
+++ scripts/prepare-app.sh
@@ -6,6 +6,9 @@ set -a; [ -f .env ] && source .env; set +a
 # Set up GIT
 source scripts/set-up-git.sh
 
+# replace %{DOCUMENT_ROOT} in public/.htaccess as it is misconfigured in deploy now
+sed -i 's|%{DOCUMENT_ROOT}|'"$HOME"'/public/|g' public/.htaccess
+
 # Reset cache and start app
 php artisan up
 php artisan config:cache
